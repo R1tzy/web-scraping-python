4f42d90fc1e431f3910545d540d26e955280a76d
R1tzy <fox_raposa64@hotmail.com>
2023-09-30 19:26:43 -0300

    update

--- cap. 9/exe4.py
+++ cap. 9/exe4.py
@@ -1,9 +1,24 @@
 from nltk import word_tokenize, sent_tokenize, pos_tag
 
+"""
+Nesse exemplo estamos usando a biblioteca 
+NLTK (Natural Language Toolkit) para realizar 
+tokenização de palavras e frases, bem como a 
+marcação POS (part-of-speech) para identificar 
+e extrair palavras específicas com determinadas 
+classes gramaticais em um texto
+"""
+
+
+# Divide o texto em sentenças
 sentences = sent_tokenize('Google is one of the best companies in the world.'\
 ' I constantly google myself to see what I\'m up to.')
 
 nouns = ['NN', 'NNS', 'NNP', 'NNPS']
+# NN = Substantivo, singular ou incontável
+# NNS = Substantivo, plural
+# NNP = Substantivo próprio, singular
+# NNPS =  Substantivo próprio, plural
 
 for sentence in sentences:
     if 'google' in sentence.lower():
@@ -12,8 +27,12 @@ for sentence in sentences:
             if word[0].lower() == 'google' and word[1] in nouns:
                 print(sentence)
 
+
 """
-Foi explicado um pouco sobre o nltk e 
-feito alguns exemplos de como utilizar 
-e por que usar
+Identifica frases que contenham a 
+palavra 'google' como um substantivo 
+nas classes gramaticais especificadas 
+em nouns. É um exemplo de como a NLTK 
+pode ser usada para análise linguística 
+em texto.
 """
